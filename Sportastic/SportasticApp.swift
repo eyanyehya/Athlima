986831a6eed7b2fbb43b78114bf6732be999a62f
eyanyehya <eyanyehya@gmail.com>
2022-09-18 21:13:29 -0400

    Initial Commit

---
+++ Sportastic/SportasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportasticApp.swift
+//  Sportastic
+//
+//  Created by Eyan Yehya on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SportasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
